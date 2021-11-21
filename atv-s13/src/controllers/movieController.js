8a1ad14bcbfbf775ed3cf3d178d726b460b55f55
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-11-21 13:52:36 -0300

    adiciona metodos put, patch e delete

--- atv-s13/src/controllers/movieController.js
+++ atv-s13/src/controllers/movieController.js
@@ -42,7 +42,86 @@ const postNewMovie = async (req, res) => {
     }
 }
 
+//[PATCH]/filmes/updateTitle?{id}
+const patchUpdateTitleById = async (req, res) => {
+    
+    try {
+
+        let movieFound = await MovieSchema.findById(req.params.id);
+        console.log(movieFound);
+
+        let newTitle = req.body.Title;
+                
+        if(movieFound){
+            movieFound.Title = newTitle;
+        }
+
+        const savedMovie = await movieFound.save();
+
+        res.status(200).json(savedMovie); 
+
+    } catch(error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+//[PUT]/filmes/update/{id}
+const updateById = async (req, res) => {
+    try {
+        //pegar o id que vem da request
+        //encontrar o filme que tem esse id
+        const foundMovie = await MovieSchema.findById(req.params.id);
+        
+        //pegar o req do body
+        //atualizar as informções
+        if(foundMovie){
+            foundMovie.Title = req.body.Title || foundMovie.Title;
+            foundMovie.Year = req.body.Year || foundMovie.Year;
+            foundMovie.Rated = req.body.Rated || foundMovie.Rated;
+            foundMovie.Released = req.body.Released || foundMovie.Released;
+            foundMovie.Runtime = req.body.Runtime || foundMovie.Runtime;
+            foundMovie.Genre = req.body.Genre || foundMovie.Genre;
+            foundMovie.Director = req.body.Director || foundMovie.Director;
+            foundMovie.Writer = req.body.Writer || foundMovie.Writer;
+            foundMovie.Actors = req.body.Actors || foundMovie.Actors;
+            foundMovie.Plot = req.body.Plot || foundMovie.Plot;
+            foundMovie.Language = req.body.Language || foundMovie.Language;
+            foundMovie.Country = req.body.Country || foundMovie.Country;
+            foundMovie.Awards = req.body.Awards || foundMovie.Awards;
+        }    
+        
+        //salvar as informações atualizadas
+        const savedMovie = await foundMovie.save();
+
+        //mostrar as informações atualizadas
+        res.status(200).json(savedMovie);
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+    
+}
+
+const deleteMovie = async (req, res) => {
+    try {
+        const foundMovie = await MovieSchema.findById(req.params.id);
+        
+        await foundMovie.delete();
+
+        res.status(204);
+        
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
+
+}
+
 module.exports = {
     getAll,
-    postNewMovie
+    postNewMovie,
+    patchUpdateTitleById,
+    updateById,
+    deleteMovie
 };
--- atv-s13/src/routes/movieRoutes.js
+++ atv-s13/src/routes/movieRoutes.js
@@ -3,8 +3,29 @@ const express = require('express');
 
 const router = express.Router();
 
+//@router GET /api/movies/
+//@desc Listar todos os filmes
+//@access Public
 router.get('/', movieController.getAll);
 
+//@router POST /api/movies/create
+//@desc Adicionar um novo filme
+//@access Public
 router.post('/create', movieController.postNewMovie);
+
+//@router PATCH /api/movies/updatetitle/:id
+//@desc Atualizar o título de um filme
+//@access Public
+router.patch('/updatetitle/:id', movieController.patchUpdateTitleById);
+
+//@router PUT /api/movies/update/:id
+//@desc Atualizar qualquer informação, menos o id
+//@access Public
+router.put('/update/:id', movieController.updateById);
+
+//@router DELETE /api/movies/delete/:id
+//@desc Deletar filme
+//@access Public
+router.delete('/delete/:id', movieController.deleteMovie);
 
 module.exports = router;
